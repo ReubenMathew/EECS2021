854348b12e677efc87a1d4f6b323624352d7b656
Reuben Mathew <intelphysic@gmail.com>
2019-09-16 15:32:32 -0400

    labA complete

---
+++ labA/aex4d.asm
@@ -0,0 +1,12 @@
+;0x1234587811223333
+addi x5, x0, 0x123
+slli x6, x5, 12
+addi x7, x6, 0x458
+slli x8, x7, 12
+addi x9, x8, 0x781
+slli x10, x9, 12
+addi x11, x10, 0x122
+slli x12, x11, 12
+addi x13, x12, 0x333
+slli x14, x13, 4
+addi x15, x14, 3
